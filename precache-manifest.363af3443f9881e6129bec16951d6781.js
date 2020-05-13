d6ad311c62a7515293d41544875bead89e4b0e16
creinoso7982 <creinoso7982@gmail.com>
2020-05-13 14:09:54 -0500

    Updates

---
+++ precache-manifest.363af3443f9881e6129bec16951d6781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c170eff7bf4571d3530ab1aef52ff7",
+    "url": "/tictactoe.git/index.html"
+  },
+  {
+    "revision": "3ca32febb74d8cb31901",
+    "url": "/tictactoe.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13e091b452ecf35f99bb",
+    "url": "/tictactoe.git/static/js/2.919c6308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe.git/static/js/2.919c6308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca32febb74d8cb31901",
+    "url": "/tictactoe.git/static/js/main.216f4169.chunk.js"
+  },
+  {
+    "revision": "a147a432607bcb19a2bf",
+    "url": "/tictactoe.git/static/js/runtime-main.28c3b0e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
